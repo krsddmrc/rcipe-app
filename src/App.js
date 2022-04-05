0593891c44374ceca574b182671bf4577ab14546
krsddmrc <k2rdnz@gmail.com>
2022-04-05 14:41:14 +0300

    ...is still growing

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Recipe from "./components/pages/Recipe";
 import About from "./components/pages/About";
+import Github from "./components/pages/Github";
+import Logout from "./components/pages/Logout";
+import Header from './components/pages/Header';
 
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar/>
+          <Header/>  
+          <Navbar/>
         <Routes>
           <Route path="/" element={<Recipe/>} />
           <Route path="/About" element={<About/>} />
-        </Routes>                    
+          <Route path="/Github" element={<Github/>} />
+          <Route path="/Logout" element={<Logout/>} />
+        </Routes>                      
       </BrowserRouter>
     </div>
   );
---
+++ src/components/pages/Github.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Github = () => {
+  return (
+    <div>GITHUB</div>
+  )
+}
+
+export default Github;
