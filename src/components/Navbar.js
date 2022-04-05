5b26c224ff842529794634969ce897db918e879d
krsddmrc <k2rdnz@gmail.com>
2022-04-05 12:16:19 +0300

    going on

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <a href="/">RECIPE</a>
+      <a href="/about">ABOUT</a>
+      <a href="/github">GITHUB</a>
+      <a href="/logout">LOGOUT</a>
+    </div>
+  )
+}
+export default Navbar;
